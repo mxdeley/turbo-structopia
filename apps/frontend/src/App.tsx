13c77762194a9004bd7846e9b6ac728c0276dcc3
mxdeley <dev@madeleydesignstudio.org>
2024-05-23 23:40:15 +0100

    added vite

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Card } from "../../../packages/ui/src/card";
+
+function App() {
+  return (
+    <>
+      <Card title="Card Title" href="https://example.com">
+        Card Content
+      </Card>
+    </>
+  );
+}
+
+export default App;
